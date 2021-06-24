178c323305ec27c06ab950787a8f2640bd451013
iiizadora <oliveiras.izadora@gmail.com>
2021-06-23 22:57:33 -0300

    add localStorage

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
 //Validação para quando estiver vazio
 const button = document.querySelector('#btn');
 const movieType = document.querySelector('#movie-type');
-const movieTaskBox = document.querySelector('movie__tasks');
+const movieTaskBox = document.querySelector('#movie__tasks');
+const buttonDel = document.querySelector('#btn-delete')
+
+
+
+
+//funtion para adicionar um novo filme
+function addNewMovies(textoNovo){
+  const addNewItem = document.createElement('span');
+  addNewItem.setAttribute('class','namemovie-task');
+  
+  addNewItem.innerHTML =textoNovo
+
+  movieTaskBox.appendChild(addNewItem);
+  
+  return addNewItem;
+}
+
 
 
 button.addEventListener('click',()=>{
 
-  if(movieType.value == 0) {
-    alert('Por favor, digite um nome de filme!')
-  } else{
-    newMovie();
+  if(movieType.value == "") {
+    alert('Por favor, digite um título de filme!')
+  } else {
+    addNewMovies(movieType.value);
+    
   }
+
+  movieType.value = "";
+  movieType.focus();
+});
+
+
+// Atualiza a página e limpa as escolhas
+buttonDel.addEventListener("click",()=>{
+  document.location.reload();
 })
 
-//funtion para adicionar um novo filme
+//Local Storage
+let saveInformation = ()=> {
 
-function newMovie(movieTask){
-  
-  const nameMovie = document.createAttribute('span');
-  nameMovie.setAttribute('class','namemovie-task');
-  nameMovie.innerHTML = movieTask;
-  movieTaskBox.addEventListener(nameMovie);
-
-  return nameMovie;
+  let movieItem = movieType.value;
+  localStorage.setItem('TitleMovie', movieItem)
 }
+
+ document.onchange= saveInformation;
+ 
